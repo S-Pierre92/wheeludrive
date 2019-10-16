106c4e2a721d46835eea495833ba525fe77a214e
User <User@192.168.0.50>
2019-10-17 00:29:37 +0200

    mysql

--- wheeludrive2/src.main/com/wheeludrive2/bean/BonjourBean.java
+++ wheeludrive2/src.main/com/wheeludrive2/bean/BonjourBean.java
@@ -11,7 +11,7 @@ public class BonjourBean implements Serializable {
 
     private static final long serialVersionUID = 1L;
 
-    private String            nom;
+    private String nom;
 
     public String getNom() {
         return nom;
